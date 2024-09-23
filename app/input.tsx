ecb6bdc652a9b82c9f95ceb74fdccd8bd3ac9cb9
Simran <25629simran.2020@gmail.com>
2024-09-23 22:12:18 +0530

    Update code: Add new feature

---
+++ app/input.tsx
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+const Input = () => {
+    const [num,setNum]=useState<Number>(0)
+  return (
+    <div className="p-10 md:p-20 flex flex-col gap-5">
+       <h1 className="font-medium text-3xl">EMB-AI BRD Studio</h1>
+     <p className="text-sm">This app generates a comprehensive Business Requirements Document (BRD) based on your inputs. Fill in the fields below and click 'Generate BRD' to create your document.</p>
+      <p>Form Completion: /4 fields</p>
+      <p>ghii</p>
+    </div>
+  )
+}
+
+export default Input
